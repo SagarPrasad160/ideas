3931adc8329ae9f8d575d450285aa458296e681f
SagarPrasad160 <sagarprasad916@gmail.com>
2023-07-04 14:21:29 +0530

    Add REST API from performing CRUD with ideas

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const ideasRouter = require("./routes/ideas");
+
+const connectDB = require("./config/db");
+connectDB();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/ideas", ideasRouter);
+
+app.listen(5000, () => {
+  console.log("Listenting at port 5000");
+});
